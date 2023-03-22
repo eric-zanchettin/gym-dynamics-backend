a81989f7c53df3b05ad5b639c278858996dd1e3d
eric-zanchettin <canalmontrisz@hotmail.com>
2023-03-21 23:26:58 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import fastify, { FastifyRequest, FastifyReply } from 'fastify';
+import cors from '@fastify/cors';
 import { fastifyJwt } from '@fastify/jwt';
 import { appRoutes } from './routes';
 
 export const app = fastify();
 
 app.register(appRoutes);
+app.register(cors);
 app.register(fastifyJwt, {
     secret: 'g!m-dyn4m!cs',
 });
@@ -14,6 +16,6 @@ app.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply
         await request.jwtVerify();
         return;
     } catch {
-        return reply.status(401).send({message: "You don't have permission"});
+        return reply.status(401).send({ message: "You don't have permission" });
     };
 });
